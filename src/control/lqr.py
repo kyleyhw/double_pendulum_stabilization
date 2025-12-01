Fix: Correct gravity signs in LQR dynamics (Up is unstable)

src/control/lqr.py
@@ -152,8 +152,8 @@ class LQRController:
         
         b = np.array([
             F + (m1 + m2) * l1 * s1 * theta1_dot**2 + m2 * l2 * s2 * theta2_dot**2 - damping * x_dot,
-            -m2 * l1 * l2 * s12 * theta2_dot**2 + (m1 + m2) * g * l1 * s1 - damping * theta1_dot,
-            m2 * l1 * l2 * s12 * theta1_dot**2 + m2 * g * l2 * s2 - damping * theta2_dot
+            -m2 * l1 * l2 * s12 * theta2_dot**2 - (m1 + m2) * g * l1 * s1 - damping * theta1_dot,
+            m2 * l1 * l2 * s12 * theta1_dot**2 - m2 * g * l2 * s2 - damping * theta2_dot
         ])
         
         q_ddot = np.linalg.solve(M_mat, b)